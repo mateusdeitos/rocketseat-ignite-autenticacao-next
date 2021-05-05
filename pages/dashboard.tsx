e9d02a07a95c09ec2a7f7a5cafc844c6beb72684
mateusdeitos <mate.deitos@hotmail.com>
2021-05-04 21:02:49 -0300

    env variable for client side and server side

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { setupApiClient } from "../services/api";
+import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 const Dashboard = () => {
 	const { user } = useAuth();
+	useEffect(() => {
+		api.get('/me')
+			.then(response => console.log('client-side:', response.data))
+			.catch(error => console.log({ error }));
+	}, [])
 	return (<h1>{user?.email}</h1>)
 }
 
@@ -12,8 +19,8 @@ export default Dashboard;
 export const getServerSideProps = withSSRAuth(async (ctx) => {
 	const apiClient = setupApiClient(ctx);
 	const response = await apiClient.get('/me')
-	console.log(response);
-	return { 
+	console.log('server-side', response.data);
+	return {
 		props: {}
 	}
 })
--- services/api.ts
+++ services/api.ts
@@ -11,7 +11,7 @@ export const setupApiClient = (ctx = undefined) => {
 	let cookies = parseCookies(ctx);
 
 	const api = axios.create({
-		baseURL: 'http://localhost:3333',
+		baseURL: process.env.NEXT_PUBLIC_API_URL,
 		headers: {
 			Authorization: `Bearer ${cookies['nextauth.token']}`
 		}
