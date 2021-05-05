2f04aaa388580a588c1cc95e6f0c25316f91f74c
mateusdeitos <mate.deitos@hotmail.com>
2021-05-04 21:12:11 -0300

    Hook to deal with permissions client-side

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,27 @@
 import { useEffect } from "react";
 import { useAuth } from "../contexts/AuthContext";
+import { usePermissions } from "../hooks/usePermissions";
 import { setupApiClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 const Dashboard = () => {
 	const { user } = useAuth();
+	const userCanSeeMetrics = usePermissions({ permissions: ['metrics.list'], roles: ['editor', 'administrator'] });
+
+
 	useEffect(() => {
 		api.get('/me')
 			.then(response => console.log('client-side:', response.data))
 			.catch(error => console.log({ error }));
 	}, [])
-	return (<h1>{user?.email}</h1>)
+
+	return (
+		<>
+			<h1>Dashboard: {user?.email}</h1>
+			{userCanSeeMetrics && <div>Métricas</div>}
+		</>
+	)
 }
 
 export default Dashboard;
